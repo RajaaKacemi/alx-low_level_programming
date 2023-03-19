c98f60ec5be562d9e311fb7e9c3d8276bd7cc072
rajaa <rajaa01.kacemi@gmail.com>
2023-03-19 15:32:36 -0700

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program print a expression.
+ * Return 1: mean success.
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
