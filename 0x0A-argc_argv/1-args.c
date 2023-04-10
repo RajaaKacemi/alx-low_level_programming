c1c9fdbec28e4b6d64322a69b4319ba87dcb80cc
rajaa <rajaa01.kacemi@gmail.com>
2023-04-10 07:00:09 -0700

    task 4

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,8 @@
  * Return: Always 0(Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void) argv; /*Ignore argv*/
 	printf("%d\n", argc);
 	return (0);
 }
