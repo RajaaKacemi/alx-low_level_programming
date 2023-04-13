18897a1f3a98491bdcf36a5142cd8566e0e04093
rajaa <rajaa01.kacemi@gmail.com>
2023-04-13 06:46:22 -0700

    task 1 after correction errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,6 +6,7 @@
  *string_nconcat - a fonction concat two string
  *@s1: the first string
  *@s2: the second string
+ *@n: the number of cases frome @s2
  *Return: a pointr (success), (echec): null
  */
 
@@ -14,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j;
 	char *ptr;
 
-	while(s2[i] != '\0')
+	while (s2[i] != '\0')
 		i++;
-	if ( n >= i)
+	if (n >= i)
 		ptr = malloc(strlen(s1) + i + 1);
 	else
 		ptr = malloc(strlen(s1) + n + 1);
@@ -27,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 		i++;
 	}
-	while(j < n)
+	while (j < n)
 	{
 		ptr[i] = s2[j];
 		i++;
