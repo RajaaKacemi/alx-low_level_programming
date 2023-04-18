7ece8fe4dc253a816a0ffab77e32a8fffb48506c
rajaa <rajaa01.kacemi@gmail.com>
2023-04-18 15:42:51 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,15 +6,28 @@
   * @age: second member
   * @owner: third member
   *
-  * Description: a struct contain 3 members, the first for the name of the dog
-  * the second for its age and the last the name of its owner.
+  * Description: struct dog
   */
 
 struct dog
 {
-	char *name[20];
-	int age;
-	char *owner[20];
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+*/ 
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 
 #endif
