fd9e6a7c4d4dee8ac63104ef7c019c7ba45eeae2
rajaa <rajaa01.kacemi@gmail.com>
2023-04-30 13:20:26 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (h == NULL)
-	{
+	if (!h->str)
 		printf("[0] (nil)\n");
-		return (count);
-	}
 	else
 	{
 		printf("%s\n", h->str);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
