53fad7bb83fe7449ab26e0c6b31d4fdca4aa157e
rajaa <rajaa01.kacemi@gmail.com>
2023-05-10 15:44:41 -0700

    task 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, Bytes, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	while (*text_content++)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1)
 		return (-1);
+
+	if (len)
+	Bytes = write(fd, text_content, len);
 
 	close(fd);
 
